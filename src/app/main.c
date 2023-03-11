b03b372bdd5749802a943e8d9d8a1450546d0663
TakitaNirasa <45029320+TakitaNirasa@users.noreply.github.com>
2023-03-11 22:40:47 +0300

    Переделал выделение памяти на проход с поиском свободного места. free в процессе

--- src/app/main.c
+++ src/app/main.c
@@ -7,7 +7,7 @@ void show_ptr (void* data, size_t size)
     for (size_t i = 0; i < size; i++)
     {
         if ((i & 7) == 0)
-    		printf ("\r\n 0x%02lx ", i+1);
+    		printf ("\r\n %p ", &((uint64_t*)data)[i]);//i+1);
         printf ("0x%08lx\t", ((uint64_t*)data)[i]);
     }
     printf ("\r\n");
@@ -20,6 +20,13 @@ int main(void)
 	uint64_t mass[100];
 
 	heapInit (mass, sizeof (mass) / sizeof (size_t));
+	show_ptr (mass, sizeof (mass) / sizeof (size_t));
+    uint64_t* ptr = (uint64_t *)heapAlloc (10);
+	show_ptr (mass, sizeof (mass) / sizeof (size_t));
+    uint64_t* ptr1 = (uint64_t *)heapAlloc (11);
+    heapFree (ptr);
+    ptr = (uint64_t *)heapAlloc (4);
+    ptr = (uint64_t *)heapAlloc (1);
 	show_ptr (mass, sizeof (mass) / sizeof (size_t));
 
     return 0;
--- src/libs/heap.c
+++ src/libs/heap.c
@@ -1,16 +1,17 @@
 #include "../include/heap.h"
 
 #include "string.h"
+#include "stdio.h"
 
-#pragma pack (push, 1)
 /**
  * @brief Структура служебной информации блока данных
  */
+#pragma pack (push, 1)
 typedef struct mem_s
 {
-    void* this;			/*<! Указатель на данную структуру		*/
-    void* next;			/*<! Указатель на следующую структуру	*/
-    block_size_t size;	/*<! Размер выделяемых данных			*/
+    block_size_t* memBlock;		/*<! Указатель на данную структуру		*/
+    block_size_t* next;			/*<! Указатель на следующую структуру	*/
+    block_size_t size;			/*<! Размер выделяемых данных			*/
 }mem_t;
 #pragma pack (pop)
 
@@ -26,7 +27,27 @@ const block_size_t memStructSize = (sizeof (mem_t) / sizeof (block_size_t)) +
 /**
  * @brief Указатель на начало кучи
  */
-static block_size_t* heapPtr;
+static block_size_t* heapPtr = NULL;
+
+/**
+ * @brief Функция добавления блока в память
+ * @param ptr Указатель на начало выделяемой памяти
+ * @param size Размер требуемой к выделению памяти
+ * @return Указатель на память, доступную для пользователя
+ */
+static block_size_t* putBlock (void* ptr, block_size_t size)
+{
+	mem_t block = {
+        .memBlock = ptr,
+        .next = (block_size_t*)ptr + memStructSize + size,
+        .size = size
+    };
+	
+	memset(ptr, 0, size);
+	memcpy(ptr, &block, sizeof (mem_t));
+	return (block_size_t*)ptr + memStructSize;
+
+}
 
 void heapInit (void* heap, block_size_t size)
 {
@@ -35,8 +56,8 @@ void heapInit (void* heap, block_size_t size)
 		return;
 	// Блок начала разметки памяти
 	mem_t intro = {
-        .this = heap,
-        .next = heap + memStructSize,
+        .memBlock = heap,
+        .next = (block_size_t*)heap + memStructSize,
         .size = size
     };
 	heapPtr = (block_size_t*)heap;
@@ -48,33 +69,57 @@ block_size_t* heapAlloc (block_size_t size)
 {
 	if (heapPtr == NULL || size == 0)
 		return NULL;
-
+	// Указатель на первый блок памяти
 	const mem_t* mem = (mem_t*)heapPtr;
 
 	// Если начальный указатель не инициализирован и указателя на следующий блок нет
-	if (mem->this != mem || mem->next != mem->this + memStructSize)
+	if ((mem_t*)mem->memBlock != mem || mem->next != mem->memBlock + memStructSize)
 		return NULL;
-	
+
+	// Полный размер памяти
 	block_size_t heapSize = mem->size;
 
-	const block_size_t* endPtr = (block_size_t*)(mem + size);
+	if (size > heapSize)
+		return NULL;
 
-	for (mem_t* ptr = (mem_t*)mem->next; ptr < (mem_t*)endPtr;)
+	// Указатель на конец кучи
+	const block_size_t* endPtr = (block_size_t*)mem + heapSize;
+
+	for (mem_t* blockPtr = (mem_t*)mem->next; blockPtr < (mem_t*)endPtr;)
 	{
 		// Если следующий элемент существует
-		if (ptr->next == ((mem_t*)(ptr->next))->this)
-		{
-			ptr = ptr->next;
-		}
+		if (blockPtr->memBlock != NULL && blockPtr->memBlock == (block_size_t*)blockPtr)
+			blockPtr = (mem_t*)(blockPtr->next);
 		else
 		{
-
+			// Проход по элементам в поиске следующего (если места хватает, то выделение памяти)
+			block_size_t* ptr = (block_size_t*)blockPtr;
+			for (;ptr < endPtr; ptr++)
+			{
+				// Найден указатель на следующий элемент
+				if (((mem_t*)ptr)->memBlock == ptr)
+					break;
+				// Если хватает места для помещения блока памяти со структурой блока	
+				else if (ptr - (block_size_t*)blockPtr <= (size + sizeof (block_size_t)) + memStructSize)
+					return putBlock (ptr, size);
+			}	
 		}
-
 	}
 	return NULL;	
 }
 
 void heapFree (block_size_t* mem)
 {
+	printf ("sizeptr %p \r\n", mem - 3);
+	if (heapPtr == NULL || mem == NULL || mem < heapPtr + (memStructSize * 2))
+		return;
+
+	mem_t* infoBlock = (mem_t*)(mem-3);
+	
+	// Если блок существует и правильно инициализирован
+	//if (infoBlock->memBlock != NULL &&  infoBlock->memBlock == infoBlock && 
+	//	infoBlock->next - infoBlock->memBlock)
+
+	block_size_t size = (((mem_t*)(mem-3))->next - ((mem_t*)(mem-3))->memBlock) * sizeof (block_size_t);
+	memset(infoBlock, 0, size);
 }
